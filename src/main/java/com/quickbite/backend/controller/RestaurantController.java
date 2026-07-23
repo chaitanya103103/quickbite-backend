af8cd222056bc81f33962389bfaab3ae5185f0c6
Chaitanya Mahajan <chaitanaya103107@gmail.com>
2026-07-23 17:17:13 +0530

    Created first API to save resturant.

--- src/main/java/com/quickbite/backend/controller/RestaurantController.java
+++ src/main/java/com/quickbite/backend/controller/RestaurantController.java
@@ -1,4 +1,23 @@
 package com.quickbite.backend.controller;
 
+
+import com.quickbite.backend.entity.Restaurant;
+import com.quickbite.backend.service.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/restaurants")
 public class RestaurantController {
+
+    @Autowired
+    private RestaurantService restaurantService;
+
+    @PostMapping
+    public void createRestaurant(@RequestBody Restaurant restaurant){
+        restaurantService.saveRestaurant(restaurant);
+    }
 }
